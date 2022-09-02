ace025e3f5ae6a497c29e5b351ecefae9404cb2f
Clara Moreira <anacacamm@gmail.com>
2022-09-02 12:08:07 -0300

    create SearchForm component

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,18 @@
+import { MagnifyingGlass } from "phosphor-react";
+import { SearchFormContainer } from "./styles";
+
+export function SearchForm() {
+    return (
+        <SearchFormContainer>
+            <input
+                type="text"
+                placeholder="Busque por transações"
+            />
+
+        <button type="submit">
+            <MagnifyingGlass size={20} />
+            Buscar
+        </button>
+    </SearchFormContainer>
+    )
+}
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -9,33 +10,34 @@ export function Transactions() {
             <Summary />
 
             <TransactionsContainer>
+                <SearchForm />
 
-        <TransactionsTable>
-          <tbody>
-                <tr>
-                  <td width="50%">Desenvolvimento de site</td>
-                  <td>
-                    <PriceHighlight variant="income">
-                      R$ 12.000,00
-                    </PriceHighlight>
-                  </td>
-                  <td>Venda</td>
-                  <td>13/04/2022</td>
-                </tr>
-                
-                <tr>
-                  <td width="50%">Desenvolvimento de site</td>
-                  <td>
-                    <PriceHighlight variant="outcome">
-                      - R$ 49,90
-                    </PriceHighlight>
-                  </td>
-                  <td>Alimentação</td>
-                  <td>15/04/2022</td>
-                </tr>
-          </tbody>
-        </TransactionsTable>
-      </TransactionsContainer>
+                <TransactionsTable>
+                <tbody>
+                        <tr>
+                        <td width="50%">Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighlight variant="income">
+                            R$ 12.000,00
+                            </PriceHighlight>
+                        </td>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                        </tr>
+                        
+                        <tr>
+                        <td width="50%">Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighlight variant="outcome">
+                            - R$ 49,90
+                            </PriceHighlight>
+                        </td>
+                        <td>Alimentação</td>
+                        <td>15/04/2022</td>
+                        </tr>
+                </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
