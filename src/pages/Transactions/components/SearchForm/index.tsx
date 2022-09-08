e14b8762bd5e436be21c242e600612e14546ba72
Clara Moreira <anacacamm@gmail.com>
2022-09-08 11:37:03 -0300

    search transactions

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -4,6 +4,8 @@ import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { SearchFormContainer } from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
     query: z.string()
@@ -12,6 +14,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const {} = useContext(TransactionsContext)
+
     const { 
         register, 
         handleSubmit,
@@ -21,9 +25,7 @@ export function SearchForm() {
     })
 
     async function handleSearchTransactions(data: SearchFormInputs) {
-        await new Promise(resolve => setTimeout(resolve, 2000))
-
-        console.log(data)
+        await fetchTransactions(data.query)
     }
 
     return (
@@ -40,4 +42,8 @@ export function SearchForm() {
         </button>
     </SearchFormContainer>
     )
+}
+
+function fetchTransactions(query: string) {
+    throw new Error("Function not implemented.");
 }
